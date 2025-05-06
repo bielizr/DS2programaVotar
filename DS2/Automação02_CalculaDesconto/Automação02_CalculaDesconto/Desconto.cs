ab6fa7443e261c01d96536d286ebdc47c4dde35a
Gabriel Callegari <gabrielcallegari789@gmail.com>
2025-05-05 22:37:10 -0300

    Add files via upload

---
+++ DS2/Automação02_CalculaDesconto/Automação02_CalculaDesconto/Desconto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automação02_CalculaDesconto
+{
+    public class Desconto
+    {
+        //Define se vai ou não ter desconto(valores >=350 têm desconto
+        public bool recebeDesconto(float valor)
+        {
+            return (valor >= 350);
+        }
+    }
+}
